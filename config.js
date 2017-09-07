353cac00fe78686608f476a1993eaf3c1c0d74f2
Anthony Branze <CaBra503@users.noreply.github.com>
2017-09-07 12:13:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@
 */
 
 module.exports = {
-  consumer_key: 'HrknGf7k8SrEprkO8Z4P4flcs ',
-  consumer_secret: '30y0rYq14LzTqTDB50WD5TtA9gxYxgyxL5aE44yKsatsaYFDV6',
-  Acess_token:'905236160783327233-ZSXcBIBEXBRgd2pZ24wTB9vDljI1p0E',
-  access_token_secret:'E8P8RsyKMB7v40xjwI7Q31HrV5I6QmKrFGiaAEXA6Wwvb'
+  consumer_key: '//Insert your own here',
+  consumer_secret: '//Insert your own here',
+  Acess_token:'//Insert your own here',
+  access_token_secret:'//Insert your own here'
 
 }
